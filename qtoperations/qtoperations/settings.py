2386851685dc06af50c49f8777282cf913f02aa5
JoseBenavidesB <josebenavideb@gmail.com>
2021-11-03 20:23:26 -0600

    collectstatic local

--- qtoperations/qtoperations/settings.py
+++ qtoperations/qtoperations/settings.py
@@ -19,6 +19,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
